20b2ae9d64f829aadfe76b59ffbdd7a42b2ae332
Stan Le <stan.le1107@gmail.com>
2015-05-19 15:16:16 -0700

    its more alive?

--- server.js
+++ server.js
@@ -10,13 +10,19 @@ var app = express();
 
 server.on('request', app);
 var io = socketio(server);
-
+var board = []
 io.on('connection', function (socket) {
     // This function receives the newly connected socket.
     // This function will be called for EACH browser that connects to our server.
     console.log('A new client has connected!');
     console.log(socket.id);
+
+    board.forEach(function(el){
+        socket.emit('draw',el.start,el.end,el.strokeColor)
+    })
+
     socket.on('draw', function(start,end, strokeColor){
+        board.push({start:start, end:end, strokeColor:strokeColor})
         socket.broadcast.emit('draw',start,end,strokeColor)
     })
 
