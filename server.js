46a30e0ac8e567f17d8ae3b63342c002a813146e
Stan Le <stan.le1107@gmail.com>
2016-04-06 16:47:29 -0700

    testing webrtc

--- server.js
+++ server.js
@@ -9,33 +9,37 @@ var express = require('express');
 var app = express();
 
 server.on('request', app);
-var io = socketio(server);
+var io = require('socket.io')(http);
 var board = []
-io.on('connection', function (socket) {
+io.on('connection', function(socket) {
     // This function receives the newly connected socket.
     // This function will be called for EACH browser that connects to our server.
     console.log('A new client has connected!');
     console.log(socket.id);
 
-    board.forEach(function(el){
-        socket.emit('draw',el.start,el.end,el.strokeColor)
+    board.forEach(function(el) {
+        socket.emit('draw', el.start, el.end, el.strokeColor)
     })
 
-    socket.on('draw', function(start,end, strokeColor){
-        board.push({start:start, end:end, strokeColor:strokeColor})
-        socket.broadcast.emit('draw',start,end,strokeColor)
+    socket.on('draw', function(start, end, strokeColor) {
+        board.push({
+            start: start,
+            end: end,
+            strokeColor: strokeColor
+        })
+        socket.emit('draw', start, end, strokeColor)
     })
 
 });
 
 
-server.listen(1337, function () {
+server.listen(1337, function() {
     console.log('The server is listening on port 1337!');
 });
 
 app.use(express.static(path.join(__dirname, 'browser')));
 
-app.get('/', function (req, res) {
+app.get('/', function(req, res) {
     res.sendFile(path.join(__dirname, 'index.html'));
 });
 
