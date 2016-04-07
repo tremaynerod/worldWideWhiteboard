13265b3d5c39a75f7e72003aae3d914bbe489eab
Stan Le <stan.le1107@gmail.com>
2016-04-06 17:09:38 -0700

    test?

--- server.js
+++ server.js
@@ -6,30 +6,40 @@ var server = http.createServer();
 var express = require('express');
 var app = express();
 
+var socketio = require('socket.io');
+
 server.on('request', app);
-var io = require('socket.io')(http);
-var board = []
+
+var io = socketio(server);
+
+var draws = {};
+
 io.on('connection', function(socket) {
-    // This function receives the newly connected socket.
-    // This function will be called for EACH browser that connects to our server.
-    console.log('A new client has connected!');
-    console.log(socket.id);
 
-    board.forEach(function(el) {
-        socket.emit('draw', el.start, el.end, el.strokeColor)
-    })
+    console.log(socket.id, 'connected');
 
-    socket.on('draw', function(start, end, strokeColor) {
-        board.push({
-            start: start,
-            end: end,
-            strokeColor: strokeColor
-        })
-        socket.emit('draw', start, end, strokeColor)
-    })
+    socket.on('join awesome room', function(roomName) {
+
+        socket.join(roomName);
+
+        if (!draws[roomName]) {
+            draws[roomName] = [];
+        } else {
+            socket.emit('board', draws[roomName]);
+        }
+
+        socket.on('imDrawing', function(start, end, color) {
+            draws[roomName].push({
+                start: start,
+                end: end,
+                color: color
+            });
+            socket.broadcast.to(roomName).emit('collaboratorDraw', start, end, color);
+        });
+
+    });
 
 });
-
 
 server.listen(1337, function() {
     console.log('The server is listening on port 1337!');
@@ -37,11 +47,6 @@ server.listen(1337, function() {
 
 app.use(express.static(path.join(__dirname, 'browser')));
 
-app.get('/', function(req, res) {
+app.get('/*', function(req, res) {
     res.sendFile(path.join(__dirname, 'index.html'));
 });
-
-
-
-
-module.exports = app;
