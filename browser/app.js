da1a5705f7a99beae0257fe5475f99ced7eeba73
Stan Le <stan.le1107@gmail.com>
2016-04-08 18:31:55 -0700

    chat?

--- browser/app.js
+++ browser/app.js
@@ -17,5 +17,15 @@ socket.on('connect', function() {
     socket.on('collaboratorDraw', function(start, end, color) {
         whiteboard.draw(start, end, color);
     });
+    socket.on('chat', function(chats) {
+        chats.forEach(function(chat) {
+            $('#messages').append($('<li>').text(chat))
+        })
+    })
+    socket.on('someone sent a message', function(user, chat) {
+        console.log('hello')
+        // message = user + '[' + date + ']:' + message
+        $('#messages').append($('<li>').text(chat))
+    })
 
 });
--- server.js
+++ server.js
@@ -13,15 +13,33 @@ server.on('request', app);
 var io = socketio(server);
 
 var draws = {};
+var chatHistory = {}
 
+var chatBot = {
+    log: function(roomName) {
+        io.to(roo)
+    }
+}
 io.on('connection', function(socket) {
 
     console.log(socket.id, 'connected');
 
     socket.on('join awesome room', function(roomName) {
+        var chatBot = {
 
+                log: function() {
+                    var date = Date.now()
+                    this.bot(date)
+                },
+                bot: function(msg) {
+                    io.to(roomName).emit('someone sent a message', socket.id, msg)
+                }
+            }
+            //joining the room
         socket.join(roomName);
-
+        if (!chatHistory[roomName]) chatHistory[roomName] = []
+        else socket.emit('chat', chatHistory[roomName])
+            //whiteboard things
         if (!draws[roomName]) {
             draws[roomName] = [];
         } else {
@@ -37,6 +55,15 @@ io.on('connection', function(socket) {
             socket.broadcast.to(roomName).emit('collaboratorDraw', start, end, color);
         });
 
+        //chat things
+        socket.on('im sending a message', function(chat) {
+            if (chat[0] === '!') {
+                chatBot[chat.slice(1)]()
+            } else {
+                chatHistory[roomName].push(chat)
+                io.to(roomName).emit('someone sent a message', socket.id, chat);
+            }
+        });
     });
 
 });
